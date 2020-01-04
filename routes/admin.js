b21bd90ad734196af04bf80a530adea34bc7204f
James Gary <gmarchesi888@gmail.com>
2020-01-04 14:21:04 -0500

    Proceeding with styling.

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const express = require("express");
 
+const rootDir = require("../helpers/path");
+
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("Request received", req.url, req.method);
-  res
-    .status(200)
-    .sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.status(200).sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
